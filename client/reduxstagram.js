486e4864ff2885d46dcb931d736a169460eda72a
MayankSrivastav <mayanksri18@yahoo.in>
2017-04-02 12:26:29 +0530

    Added code to implement react router

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -3,10 +3,26 @@ import {render} from 'react-dom';
 
 // import css
 import css from './styles/style.styl';
+
+// import components
 import Main from './components/main.js';
+import SinglePhoto from './components/singlePhoto.js';
+import PhotoGrid from './components/photoGrid';
+
+//import React router
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view:postId" component={SinglePhoto}></Route>
+    </Route>
+  </Router>
+);
 
 // Main render method for React component
 render(
-  <Main />,
+  router,
   document.getElementById('root')
 );
