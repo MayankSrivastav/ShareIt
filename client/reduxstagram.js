ffa3d5ef43d86aba072e3ee14a4d4c202c8546c6
MayankSrivastav <mayanksri18@yahoo.in>
2017-04-02 11:32:00 +0530

    Initial render check

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,11 @@
-// let's go!
+import React from 'react';
+import {render} from 'react-dom';
+
+// import css
+import css from './styles/style.styl';
+
+// Main render method for React component
+render(
+    <p>Hello</p>,
+    document.getElementById('root')
+);
