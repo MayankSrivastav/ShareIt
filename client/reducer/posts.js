aab4fd5fa57186b2a6988b793c79dfeb813c56d4
MayankSrivastav <mayanksri18@yahoo.in>
2017-04-03 22:16:25 +0530

    added posts component

---
+++ client/reducer/posts.js
@@ -0,0 +1,9 @@
+// reducer file for posts
+// input: the action, copy of the current state
+
+function posts(state = [], action) {
+  console.log(state, action);
+  return state;
+}
+
+export default posts;
