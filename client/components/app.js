b629cb5f5a9bfecc5862dcb7cc89d0f054a1452b
MayankSrivastav <mayanksri18@yahoo.in>
2017-04-03 22:43:27 +0530

    Connected store to the component

--- client/components/app.js
+++ client/components/app.js
@@ -1,4 +1,20 @@
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import * as actionCreators from '../actions/actionCreators.js';
-import main from './main.js';
+import Main from './main.js';
+
+function mapStateToProps(state) {
+  return {
+    props: state.posts,
+    comments: state.comments
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(actionCreators, dispatch);
+}
+
+// connect component to redux store
+const App = connect(mapStateToProps, mapDispatchToProps);
+
+export default App;
