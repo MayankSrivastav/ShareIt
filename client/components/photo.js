62d45f36c54d30ec701271ee324d865a8ab3a16f
MayankSrivastav <mayanksri18@yahoo.in>
2017-04-03 22:59:30 +0530

    added Photo component

--- client/components/photo.js
+++ client/components/photo.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+const Photo = React.createClass({
+  render() {
+    return (
+      <figure className="grid-figure">
+        It's a Photo
+      </figure>
+    );
+  }
+});
+
+export default Photo;
