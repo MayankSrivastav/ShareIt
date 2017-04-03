932fbfb97d12f3f45d1efac7cdaaf916f2a8fb90
MayankSrivastav <mayanksri18@yahoo.in>
2017-04-03 22:15:43 +0530

    Added comments component

---
+++ client/reducer/comments.js
@@ -0,0 +1,9 @@
+// reducer file for comments
+// input: the action, copy of the current state
+
+function comments(state = [], action) {
+  console.log(state, action);
+  return state;
+}
+
+export default comments;
