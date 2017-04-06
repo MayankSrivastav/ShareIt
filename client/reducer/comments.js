857350d73a8d5ed31758b04f0a8a2da0400c4916
MayankSrivastav <mayanksri18@yahoo.in>
2017-04-06 21:20:27 +0530

    Updating state with the new comment

--- client/reducer/comments.js
+++ client/reducer/comments.js
@@ -1,8 +1,25 @@
 // reducer file for comments
 // input: the action, copy of the current state
 
+function postComments(state = [], action) {
+  switch(action.type) {
+    case 'ADD_COMMENT':
+      // return the new state with new comments
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+  }
+  return state;
+}
+
 function comments(state = [], action) {
-  console.log(state, action);
+  if (typeof action.postId !== 'undefined') {
+    return {
+      ...state,
+      [action.postId] : postComments(state[action.postId], action)
+    };
+  }
   return state;
 }
 
