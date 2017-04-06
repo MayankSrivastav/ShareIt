72766c816f0b5bf16921b2f97343c289d0e74830
MayankSrivastav <mayanksri18@yahoo.in>
2017-04-06 20:40:28 +0530

    Implemented comments display in singlePhoto

--- client/components/comments.js
+++ client/components/comments.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 const Comments = React.createClass({
+  renderComments(comment, ind) {
+    return (
+      <div className="comment" key={ind}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    );
+  },
+
   render(){
     return (
-      <div className="comment">
-        I'm the comments
+      <div className="comments">
+        {this.props.postComments.map(this.renderComments)}
       </div>
     );
   }
